fb2ad1343918b105b617883445f0789922d2df77
Tim Kao <pakuya.kao@gmail.com>
2021-07-24 11:45:03 -0400

    init function and go slice

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // a function whose name starts with a capital letter can be called by a function not in the same package.
@@ -13,8 +15,25 @@ func Hello(name string) (string, error) {
 	}
 
 	// := operator is a shortcut for declaring and initializing a variable in one line
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 
 	// nil means no error
 	return message, nil
+}
+
+// Go executes init functions automatically at program startup, after global variables have been initialized.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+
+	// define a slice. A slice is like an array, except that its size changes dynamically as you add and remove items.
+	formats := []string{
+		"Hi, %v, Welcome",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
