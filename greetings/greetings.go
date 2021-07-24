feac073b1c40a41299c54c978388442d509e7291
Tim Kao <pakuya.kao@gmail.com>
2021-07-24 11:28:46 -0400

    return multiple value types, use log and error package

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,20 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // a function whose name starts with a capital letter can be called by a function not in the same package.
-func Hello(name string) string {
+// note that the func returns multiple types now
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// := operator is a shortcut for declaring and initializing a variable in one line
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+
+	// nil means no error
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Tim")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Tim")
+	if err != nil {
+		// Fatal will stop the program immediately
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
